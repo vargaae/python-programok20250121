eb58e35a84c8e668b55b193e2e85fd9ec4b4931a
ANDRAS VARGA <vargaae@hotmail.com>
2025-01-22 00:19:02 +0100

    clean Filmek class

--- filmek.py
+++ filmek.py
@@ -2,7 +2,5 @@ class Filmek:
     def __init__(self, sor:str):
         tmp:list[str] = sor.strip().split(';')
         self.film_cim:str = tmp[0]
-        self.ev:int = tmp[1]
-        self.title = tmp[0]
-        self.year = int(tmp[1])
-        self.mufajok = tmp[2].split(", ")
+        self.ev:int = int(tmp[1])
+        self.mufajok:list[str] = tmp[2].split(", ")
